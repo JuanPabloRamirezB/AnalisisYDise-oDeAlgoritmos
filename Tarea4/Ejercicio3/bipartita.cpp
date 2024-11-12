32dc1c0bc0970a5c9f44fb8c70f0072ad9f5614e
JuanPabloRamirezB <86618784+JuanPabloRamirezB@users.noreply.github.com>
2024-11-12 17:35:14 -0600

    Update bipartita.cpp

--- Tarea4/Ejercicio3/bipartita.cpp
+++ Tarea4/Ejercicio3/bipartita.cpp
@@ -3,24 +3,31 @@
 #include <queue>
 #include <fstream>
 #include <ctime>
-using namespace std;
+//using namespace std;
 
 #define MAX 500
-vector<int> ady[MAX];  // Lista de adyacencia
-int V, E;              // Número de vértices y aristas
+std::vector<std::pair<int, int>> ady[MAX];  // Lista de adyacencia
+//std::vector<int> ady[MAX];                  // Lista de adyacencia
+int V, E;                                   // Número de vértices y aristas
 
-bool isBipartite(int src) {
-    vector<int> color(V + 1, -1); // Inicializamos el color de todos los vértices a -1
-    queue<int> q;
+bool isBipartite(int i) {
+    std::vector<int> color(V + 1, -1); // Inicializamos el color de todos los vértices a -1
+    std::queue<int> q;
+    //printf("src: %d\n", i);
 
-    color[src] = 1; // Asignamos el primer color al vértice fuente
-    q.push(src);
+    color[i] = 1; // Asignamos el primer color al vértice fuente
+    q.push(i);
 
     while (!q.empty()) {
         int u = q.front();
+        //printf("u: %d\n", u);
         q.pop();
 
-        for (int v : ady[u]) {
+        for (auto& vecino : ady[u]) {
+            int v = vecino.first;
+            //printf("v: %d\n", v);
+            //printf("color[v]: %d\n", color[v]);
+            //printf("color[u]: %d\n", color[u]);
             // Si el vértice adyacente no ha sido coloreado, le asignamos un color opuesto
             if (color[v] == -1) {
                 color[v] = 1 - color[u];
@@ -37,6 +44,7 @@ bool isBipartite(int src) {
 
 bool checkBipartite() {
     for (int i = 1; i <= V; ++i) {
+        //printf("Paso: %d\n", i);
         if (ady[i].size() > 0) { // Solo comprobamos los vértices que tienen aristas
             if (!isBipartite(i)) {
                 return false;
@@ -47,33 +55,46 @@ bool checkBipartite() {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        cout << "Uso: " << argv[0] << " <nombre_archivo>" << endl;
+    if (argc != 3) {
+        std::cout << "Uso: " << argv[0] << " <nombre_archivo> <1 ponderado||2 no ponderado>" << std::endl;
         return 1;
     }
 
-    ifstream file(argv[1]);
+    std::ifstream file(argv[1]);
     if (!file.is_open()) {
-        cout << "No se pudo abrir el archivo" << endl;
+        std::cout << "No se pudo abrir el archivo" << std::endl;
         return 1;
     }
 
+    int peso = *argv[2];
+    //printf("Peso: %d\n", peso);
+    std::string line;
+    getline(file, line);
+    printf("%s\n", line.c_str());
     // Leer el número de vértices y aristas
     file >> V >> E;
-    int u, v;
+    printf("Numero de vertices: %d\nNumero de aristas: %d\n", V, E);
+    int u, v, w = 1;
     for (int i = 0; i < E; ++i) {
-        file >> u >> v;
-        ady[u].push_back(v);
-        ady[v].push_back(u); // Grafo no dirigido
+        if(peso == 49)
+            file >> u >> v >> w;
+        if(peso == 50)
+            file >> u >> v;
+
+        //printf("u: %d, v: %d\n", u, v);
+        ady[u].emplace_back(v, w); // Grafo dirigido
+        //ady[v].emplace_back(u,w); // Grafo no dirigido
+
+        //ady[u].push_back(std::make_pair(v, w)); 
     }
     file.close();
 
     clock_t start = clock();
     bool bipartite = checkBipartite();
     clock_t end = clock();
-
+    //printf("%d\n",bipartite);
     double executionTime = double(end - start) / CLOCKS_PER_SEC;
-    cout << (bipartite ? "Bipartita" : "No bipartita") << endl;
+    std::cout << (bipartite ? "\nBipartita\n" : "\nNo bipartita\n") << std::endl;
     printf("Tiempo total de ejecucion: %.6f segundos\n", executionTime);
 
     return 0;
